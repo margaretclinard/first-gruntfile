30b70e13a9ced536009bc018543557accfb4a8ee
Margaret Clinard <margaret.clinard@gmail.com>
2015-02-24 09:48:59 -0600

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    clean: ['public'],
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'app/', src: ['**', '!**/*.jade', '!**/*.{sass,scss}'], dest: 'public/', filter: 'isFile'}
+        ]
+      }
+    },
+    jade: {
+      compile: {
+        files: [{expand: true, cwd: 'app/', src: ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}]
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'public/css/main.css': 'app/styles/main.scss'
+        }
+      }
+    },
+    watch: {
+      other: {
+        files: ['app/**', '!**/*.jade', '!**/*.{sass,scss}'],
+        tasks:['copy']
+      },
+      jade: {
+        files: ['app/**/*.jade'],
+        tasks: ['jade']
+      },
+      sass: {
+        files: ['app/**/*. {sass,scss}'],
+        tasks:['sass']
+      }
+    }
+  });
+
+  grunt.registerTask('default', []);
+  grunt.registerTask('build', ['clean', 'copy', 'jade', 'sass']);
+  grunt.registerTask('serve', ['build', 'watch']);
+};
